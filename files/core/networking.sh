89a317f48b5c5a56c7b53c0b4290a344e574be89
Stu Franks <stu.j.franks@gmail.com>
2018-01-09 16:21:51 +0000

    Don't set a pri gateway if an ext network is configured with a gateway

--- files/core/networking.sh
+++ files/core/networking.sh
@@ -14,7 +14,8 @@ export HOSTNAME="<%= network.hostname %>"
 export IP="<%= network.ip %>"
 export NETMASK="<%= network.netmask %>"
 export NETWORK="<%= network.network %>"
-export GATEWAY="<%= network.gateway %>"
+# Set gateway unless an 'ext' network is defined (prevents a pri and ext gateway being set and confusing things)
+export GATEWAY="<%= if (config.networks.ext.gateway rescue false) && name.to_s != 'ext' then '' else network.gateway end %>"
 
 #Bridge Configuration
 export BRIDGE="<%= if (network.bridge rescue false) then 'Bridge' end %>"
