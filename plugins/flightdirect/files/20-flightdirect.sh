74ca3f037260248b967cd5e4016f8bbdbe452633
Stu Franks <stu.j.franks@gmail.com>
2018-01-24 17:13:36 +0000

    Flight Direct plugin

---
+++ plugins/flightdirect/files/20-flightdirect.sh
@@ -0,0 +1,65 @@
+wget -O /tmp/flight-install https://s3-eu-west-1.amazonaws.com/alces-flight/FlightDirect/1.0.2/flight-install
+
+<% if plugins.flightdirect.config.flightdirect_isclient -%>
+cat << EOF > /tmp/config.yml
+roles:
+- compute
+facilities:
+- software management
+clustername: <%= config.cluster %>
+masterip: ""
+uuid: ""
+token: ""
+hostname: ""
+hostprefix: ""
+scheduler: ""
+firewallrulepath: ""
+nfspaths: []
+nfscustompaths: ""
+gridwarepath: /opt/gridware
+sessiontypes: []
+storagetypes: []
+vpnport: ""
+vpnnetwork: ""
+vpnauth: ""
+EOF
+<% elsif plugins.flightdirect.config.flightdirect_isserver -%>
+cat << EOF > /tmp/config.yml
+roles:
+- user
+facilities:
+- software management
+- guides and templates
+- interactive sessions
+- object/file repositories
+- passwordless SSH
+clustername: <%= config.cluster %>
+masterip: ""
+uuid: ""
+token: ""
+hostname: ""
+hostprefix: ""
+scheduler: ""
+firewallrulepath: ""
+nfspaths: []
+nfscustompaths: ""
+gridwarepath: /opt/gridware
+sessiontypes:
+- gnome
+storagetypes:
+- posix
+- s3
+vpnport: ""
+vpnnetwork: ""
+vpnauth: ""
+EOF
+<% end -%>
+
+cat << EOF > /var/lib/firstrun/scripts/flightdirect.bash
+rm -rf /opt/clusterware
+export cw_FLIGHT_quiet=true
+mkdir -p /etc/xdg/flight
+cp /tmp/config.yml /etc/xdg/flight/.
+/tmp/flight-install initialize
+/opt/clusterware/opt/flight-configuration-tool/bin/flight-configure apply
+EOF
