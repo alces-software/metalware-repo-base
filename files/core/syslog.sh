ed8a307b8e7f8532ddb534c40993a79bf3104c2d
Stu Franks <stu.j.franks@gmail.com>
2018-01-26 12:51:02 +0000

    Separate config and service files from firstrun, chrony and syslog

--- files/core/syslog.sh
+++ files/core/syslog.sh
@@ -5,33 +5,16 @@
 
 yum -y install rsyslog
 <% if config.rsyslog.is_server -%>
-cat << EOF > /etc/rsyslog.d/metalware.conf
-\$template remoteMessage, "/var/log/slave/%FROMHOST%/messages.log"
-:fromhost-ip, !isequal, "127.0.0.1" ?remoteMessage
-& ~
-EOF
+install_file metalware.conf /etc/rsyslog.d/metalware.conf
 
 sed -i -e "s/^#\$ModLoad imudp.*$/\$ModLoad imudp/g" /etc/rsyslog.conf
 sed -i -e "s/^#\$UDPServerRun 514.*$/\$UDPServerRun 514/g" /etc/rsyslog.conf
 sed -i -e "s/^#\$ModLoad imtcp.*$/\$ModLoad imtcp/g" /etc/rsyslog.conf
 sed -i -e "s/^#\$InputTCPServerRun 514.*$/\$InputTCPServerRun 514/g" /etc/rsyslog.conf
 
-cat << EOF > /etc/logrotate.d/rsyslog-remote
-/var/log/slave/*/*.log {
-    sharedscripts
-    compress
-    rotate 2
-    postrotate
-        /bin/kill -HUP \`cat /var/run/syslogd.pid 2> /dev/null\` 2> /dev/null || true
-        /bin/kill -HUP \`cat /var/run/rsyslogd.pid 2> /dev/null\` 2> /dev/null || true
-    endscript
-}
-EOF
-cat << EOF > /var/lib/firstrun/scripts/firewall_rsyslog.bash
-firewall-cmd --add-port 514/udp --zone internal --permanent
-firewall-cmd --add-port 514/tcp --zone internal --permanent
-firewall-cmd --reload
-EOF
+install_file rsyslog-remote /etc/logrotate.d/rsyslog-remote
+
+install_file firewall_rsyslog.bash /var/lib/firstrun/scripts/firewall_rsyslog.bash
 <% else -%>
 echo '*.* @<%= config.rsyslog.server %>:514' >> /etc/rsyslog.conf
 <% end -%>
