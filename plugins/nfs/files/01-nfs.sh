9a722812967f20923120d0b41d0171b258f2b662
Stu Franks <stu.j.franks@gmail.com>
2018-01-29 17:26:36 +0000

    Correct 'plugin' to 'plugins'

--- plugins/nfs/files/01-nfs.sh
+++ plugins/nfs/files/01-nfs.sh
@@ -12,24 +12,24 @@ sed -ie "s/^#\RPCNFSDCOUNT.*$/\RPCNFSDCOUNT=32/g" /etc/sysconfig/nfs
 EXPORTOPTS="<%= config.networks.pri.network %>/<%= config.networks.pri.netmask %>(rw,no_root_squash,sync)"
 
 EXPORTS=`cat << EOF
-<% plugin.nfs.config.nfs_exports.each do | path, opts | -%>
+<% plugins.nfs.config.nfs_exports.each do | path, opts | -%>
 <%= path %>   <%= if defined?(opts.options) then opts.options else "#{config.networks.pri.network}/#{config.networks.pri.netmask}(rw,no_root_squash,sync)" end %>
 <% end -%>
 EOF`
 
 echo "$EXPORTS" > /etc/exports
 
-<% elsif (!plugin.nis.config.nis_isserver rescue true) -%>
+<% elsif (!plugins.nis.config.nis_isserver rescue true) -%>
 
 MOUNTS=`cat << EOF
-<% plugin.nfs.config.nfs_mounts.each do | mount, path | -%>
+<% plugins.nfs.config.nfs_mounts.each do | mount, path | -%>
 <%= path.server %>:<%= path.export %>    <%= mount %>    nfs    <%= if defined?(path.options) then path.options else 'intr,rsize=32768,wsize=32768,vers=3,_netdev' end -%>    0 0
 <% end -%>
 EOF`
 
 echo "$MOUNTS" >> /etc/fstab
 
-<% plugin.nfs.config.nfs_mounts.each do | mount, path | -%>
+<% plugins.nfs.config.nfs_mounts.each do | mount, path | -%>
 mkdir -p <%= path.target %>
 <% end -%>
 
