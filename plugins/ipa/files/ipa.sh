fe0140556bfa2adde66c708aebe71d9baa695791
Stu Franks <stu.j.franks@gmail.com>
2018-03-07 15:40:19 +0000

    Downcase hostname (otherwise causes ipa install error)

--- plugins/ipa/files/ipa.sh
+++ plugins/ipa/files/ipa.sh
@@ -13,6 +13,6 @@ yum -y install ipa-client ipa-admintools
 # Enroll host (using firstrun script)
 cat << EOF > /var/lib/firstrun/scripts/ipaenroll.bash
 REALM="<%= config.networks.pri.domain.upcase %>.<%= config.domain.upcase %>"
-ipa-client-install --no-ntp --mkhomedir --no-ssh --no-sshd --force-join --realm="$REALM" --server="<%= node.plugins.ipa.config.ipa_servername %>.<%= config.networks.pri.domain %>.<%= config.domain %>" -w "<%= node.plugins.ipa.config.ipa_insecurepassword %>" --domain="<%= config.networks.pri.domain %>.<%=config.domain %>" --unattended --hostname='<%= config.networks.pri.hostname %>'
+ipa-client-install --no-ntp --mkhomedir --no-ssh --no-sshd --force-join --realm="$REALM" --server="<%= node.plugins.ipa.config.ipa_servername %>.<%= config.networks.pri.domain %>.<%= config.domain %>" -w "<%= node.plugins.ipa.config.ipa_insecurepassword %>" --domain="<%= config.networks.pri.domain %>.<%=config.domain %>" --unattended --hostname='<%= config.networks.pri.hostname.downcase %>'
 EOF
 <% end -%>
