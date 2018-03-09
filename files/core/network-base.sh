e6940fa321b5c0572eb2b09b5275577bf5ca44b9
Stu Franks <stu.j.franks@gmail.com>
2018-03-09 15:15:59 +0000

    Add newline otherwise EOF confusion ensues

--- files/core/network-base.sh
+++ files/core/network-base.sh
@@ -10,12 +10,12 @@ echo "HOSTNAME=<%= config.networks.pri.hostname %>" >> /etc/sysconfig/network
 echo "<%= config.networks.pri.hostname %>" > /etc/hostname
 
 <% if config.dns_type.to_s == 'named' || node.name.to_s != 'local'  %>
-<%= if node.name.to_s == 'local' then 'cat << FIRSTRUN > /var/lib/firstrun/scripts/resolv.bash' end -%>
+<%= if node.name.to_s == 'local' then 'cat << FIRSTRUN > /var/lib/firstrun/scripts/resolv.bash' end %>
 cat << EOF > /etc/resolv.conf
 search <%= config.search_domains %>
 nameserver <%= config.internaldns %>
 EOF
-<%= if node.name.to_s == 'local' then 'FIRSTRUN' end -%>
+<%= if node.name.to_s == 'local' then 'FIRSTRUN' end %>
 <% else %>
 cat << EOF > /etc/resolv.conf
 search <%= config.search_domains %>
