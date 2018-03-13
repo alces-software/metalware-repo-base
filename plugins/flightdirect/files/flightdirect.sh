a5d525f40c7787bc1be23f377082681019fdc8bd
Stu Franks <stu.j.franks@gmail.com>
2018-03-13 14:33:24 +0000

    Add additional flightdirect vars in the event of localised installation

--- plugins/flightdirect/files/flightdirect.sh
+++ plugins/flightdirect/files/flightdirect.sh
@@ -56,6 +56,12 @@ EOF
 <% end -%>
 
 cat << EOF > /var/lib/firstrun/scripts/flightdirect.bash
+#export cw_BUILD_flight_configure_url=http://10.10.0.1/installers/flightdirect/flight-configure
+#export cw_BUILD_installer_url=
+#export cw_BUILD_dist_url=http://10.10.0.1/installers/flightdirect/dist
+#export cw_BUILD_source_url=http://10.10.0.1/installers/flightdirect/clusterware.tar.gz
+#export cw_BUILD_repos_url=http://10.10.0.1/installers/flightdirect/repos
+
 rm -rf /opt/clusterware
 export cw_FLIGHT_quiet=true
 mkdir -p /etc/xdg/flight
