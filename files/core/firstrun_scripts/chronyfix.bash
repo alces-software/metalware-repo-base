8bccafa2e60fdf8405e0923fdc352f9437bd70e2
stuts <stu.j.franks@gmail.com>
2018-09-10 13:06:56 +0100

    Fix chrony for flightcenter controller

--- files/core/firstrun_scripts/chronyfix.bash
+++ files/core/firstrun_scripts/chronyfix.bash
@@ -1,4 +1,8 @@
 systemctl stop chronyd
+<% if config.ntp.is_server -%>
+ntpdate <%= (node.plugins.flightcenter.config.flightcenter_ntpserver rescue false) || '0.centos.pool.ntp.org' %>
+<% else -%>
 ntpdate <%= config.ntp.server %>
+<% end -%>
 systemctl start chronyd
 hwclock --systohc
